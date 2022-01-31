c87232b882eefdcf8cc34c8a526c8e249d3da448
Leonardo <mr_kunkel@hotmail.com>
2022-01-31 11:16:18 -0600

    first route created

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+* PATORNES DE DISEÑO
+* SON PROPUESTAS Y CAOSO DE USO QUE PERMITN RESOLVER PROBLEMAS COTIDIANOS
+
+/////// MODULE PATTERN - PATRÓN MODULAR ///////////
+
+- APLICA VALIDEZ DE IMPORTACIONES Y EXPORTACIONES EN UN PROYECTO
+
+*/
+
+// 1.- IMPORTACIONES
+// PROCESO DE TRAER FUNCIONES O VARIABLES DE OTROS ARCHIVOS  JS  A ESTE
+
+// A. Archivos externos propios
+//const suma = require("./suma");
+
+// B. Archivos de node_modules
+// EXPRESS ES UN A LIBRERÍA QUE FUNCIONA PARA LA GESTIÓN DE RUTAS Y MIDDLEWARES
+const express = require("express");
+const app = express();
+
+// 2. MIDDLEWARES
+
+
+// 3. RUTAS
+app.use("/", require("./routes/index.routes"));
+
+// 4. SERVIDOR
+app.listen(3000, () => {
+    console.log(`It's alive`);
+})
---
+++ suma.js
@@ -0,0 +1,7 @@
+const suma = (a, b) => {
+    return a + b
+}
+
+// Permitir disponibilidad al público
+
+module.exports = suma
