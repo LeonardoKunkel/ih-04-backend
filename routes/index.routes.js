9704a196e13675eafc9919b449967ec0beb8f00d
Leonardo <mr_kunkel@hotmail.com>
2022-01-31 12:01:53 -0600

    new route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,16 @@
 // 1. IMPORTACIONES
 const express = require("express");
+const res = require("express/lib/response");
 const router = express.Router()
 
 // 2. RUTEO
 router.get("/", (req, res) => {
     res.send("Hola Mundo");
 });
+
+router.get("/contacto", () => {
+    res.send("Contacto")
+})
 
 // 3. EXPORTACIONES
 module.exports = router
